9d1f0d3a3012cc1b54234d8f613ea94e86af645a
Owen Yang <owentheyanger@gmail.com>
2024-09-23 17:48:59 -0700

    started

--- backend/rename.js
+++ backend/rename.js
@@ -1,11 +1,13 @@
 import express from "express";
 import http from "http";
 import cors from "cors";
+const app = express()
 
-const server = createServer((req, res) =>{
-    res.writeHead(200, {'content-type':'text/html'})
-    res.write('<h1>test</h1>')
-    res.end()
+app.get('/', (req, res) =>{
+    res.send('Home Page')
+})
+app.listen(5000, () => {
+    console.log('server is listening on port 5000')
 })
 
 server.listen(1234)
