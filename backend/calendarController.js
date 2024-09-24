b94039bab0f87a9af074d0bb5fb0bb206dc78e7d
Owen Yang <owentheyanger@gmail.com>
2024-09-23 17:27:56 -0700

    added headers

--- backend/calendarController.js
+++ backend/calendarController.js
@@ -1,5 +1,9 @@
 const http = require('http')
 
-const server = http.createServer((req, res) =>{})
+const server = http.createServer((req, res) =>{
+    res.writeHead(200, {'content-type':'text/html'})
+    res.write('<h1>test</h1>')
+    res.end()
+})
 
 server.listen(1234)
