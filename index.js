47e44082533e9ac837660955067625411dc01a98
Owen Yang <owentheyanger@gmail.com>
2024-09-24 17:11:03 -0700

    Express Initialization

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import http from "http";
+import path from "path"
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, './frontend')))
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./frontend", "index.html"))
+})
+
+app.listen(5000, () =>{
+    console.log('Server is running on port 5000')
+})
