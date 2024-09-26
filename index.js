d2eaf36c5d9ad53549071da9537d9fdabf8270b1
Owen Yang <owentheyanger@gmail.com>
2024-09-25 18:48:08 -0700

    configuring middleware

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import path from "path"
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, './frontend')))
-
+app.use(express.json())
 app.get('/', (req, res) => {
     res.json([{}])
 })
