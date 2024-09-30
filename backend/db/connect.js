9e4bccdfd330e719d46851ab4da547d4a1852aa2
Owen Yang <owentheyanger@gmail.com>
2024-09-29 18:49:17 -0700

    fixed db not connecting

--- backend/db/connect.js
+++ backend/db/connect.js
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
-const connectDB = async () => {
-    try{
+export const connectDB = async () => {
+    try {
+        mongoose.set('debug', true);
         const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
-    }
-    catch(err){
-        console.error(`Error: ${err.message}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
         process.exit(1);
     }
-}
-export default connectDB;
+};
--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,12 @@ import path from "path"
 import pdf from 'pdf-parse/lib/pdf-parse.js'
 import calendarRouter from "./routes/calendar.js";
 import authRouter from "./routes/auth.js";
-import connectDB from "./db/connect.js";
+import {connectDB} from "./db/connect.js";
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
-connectDB();
 
 //app.use(express.static(path.resolve(__dirname, './frontend')))
 app.use(express.urlencoded({extended: true, limit: '50 mb'}))
@@ -19,7 +18,7 @@ app.use(express.json({limit: '50 mb'}))
 
 app.use('/api/auth', authRouter)
 app.use('/api/calendar', calendarRouter)
-
+await connectDB();
 app.listen(5000, () =>{
     console.log('Server is running on port 5000')
 })
