a62a71f28790cefcdec7d0d47dba45529b527a6b
Owen Yang <owentheyanger@gmail.com>
2024-10-03 21:59:08 -0700

    added profilepic in user model

--- backend/models/users.js
+++ backend/models/users.js
@@ -13,6 +13,10 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
+    },
+    profilepic: {
+        type: String,
+        required: true
     }
 }, {timestamps: true})
 const User = mongoose.model("User", userSchema);
