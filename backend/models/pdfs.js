755666c4332d93be9d62147d65e297f3addfb6e1
Owen Yang <owentheyanger@gmail.com>
2024-10-05 21:33:01 -0700

    implemented pdf model

--- backend/models/pdfs.js
+++ backend/models/pdfs.js
@@ -1,7 +1,22 @@
 import mongoose from "mongoose";
 
 const pdfSchema = new mongoose.Schema({
-
-})
+    file: {
+        type: Buffer,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    contentType: {
+        type: String,
+        required: true
+    },
+    isGridFS: {
+        type: Boolean,
+        default: false
+    }
+}, {timestamps: true})
 const pdfModel = mongoose.model("PDF", pdfSchema)
 export default pdfModel;
