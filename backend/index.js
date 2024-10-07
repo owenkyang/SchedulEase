e74ee23060f3f2f1fece38869cd6178e6370fc46
Owen Yang <owentheyanger@gmail.com>
2024-10-06 23:04:56 -0700

    implemented port configurations

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,16 @@ let bucket;
     });
   });
 })();
-
-app.listen(5000, () =>{
-    console.log('Server is running on port 5000')
-})
+const port = process.env.PORT || 5000
+const start = async () => {
+  try{
+    await connectDB();
+    app.listen(port, (port) =>{
+      console.log(`Server is running on port ${port}`)
+    })
+  }
+  catch(error){
+    console.log(error)
+  }
+}
+start();
