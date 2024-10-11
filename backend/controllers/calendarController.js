864863412b056df75bb9157114b9cef855836b3a
Owen Yang <owentheyanger@gmail.com>
2024-10-11 13:50:05 -0700

    configured error handling

--- backend/controllers/calendarController.js
+++ backend/controllers/calendarController.js
@@ -3,12 +3,14 @@ import http from "http";
 import pdf from 'pdf-parse/lib/pdf-parse.js'
 import pdfModel from "../models/pdfs.js";
 import { GoogleApis } from "googleapis";
+
 const PdfParser = async (req, res) => {
     const {pdfData} = req.body
     if (!pdfData){
         return res.status(400).json({success:false, msg: 'Please input a valid PDF'})
     }
     try{
+        const createPDF = await pdfModel.create(req.body);
         const binary = Buffer.from(pdfData, 'base64');
         const pdfText = await pdf(binary)
         console.log("PDF sent successfully")
