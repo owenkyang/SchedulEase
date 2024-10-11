1544c4b543eac1d2497a3a4d3a2cd1dde91d53f4
Owen Yang <owentheyanger@gmail.com>
2024-10-11 14:29:46 -0700

    initializing JWT

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
 import User from "../models/users.js";
+import jwt from 'jsonwebtoken';
+
+
 
 const login = async (req, res) => {
-    
+    const createUser = await User.create(req.body)
+    const token = sign({id: createUser.id, username: createUser.username}, process.env.JWT_SECRET, {expiresIn: '30d'})
 }
 export default login;
